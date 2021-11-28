061847988e77f185cb9936dcab105a57c77b158d
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2021-11-28 12:16:51 +0100

    a

--- Button.cs
+++ Button.cs
@@ -4,41 +4,41 @@ using Raylib_cs;
 
 namespace VinterProjektet2
 {
-  public class Button
+ public class Button
+ {
+  int xPosition = 500;
+  int width = 3000;
+  int height = 80;
+
+  public Color ButtonColor;
+
+  public int yPosition;
+  public Vector2 mousePos;
+  public Sound clickSound;
+  public Rectangle button;
+  public bool areOverlapping;
+
+  virtual public void Update()
   {
-    int xPosition = 500;
-    int width = 3000;
-    int height = 80;
+   button = new Rectangle(xPosition, yPosition, width, height);
 
-    public Color ButtonColor;
+   if (areOverlapping)
+   {
+    button.x -= 12;
+    ButtonColor.a = 255;
 
-    public int yPosition;
-    public Vector2 mousePos;
-    public Sound clickSound;
-    public Rectangle button;
-    public bool areOverlapping;
-
-    virtual public void Update()
+    if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
     {
-      button = new Rectangle(xPosition, yPosition, width, height);
-
-      if (areOverlapping)
-      {
-        button.x -= 12;
-        ButtonColor.a = 255;
-
-        if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
-        {
-          Raylib.PlaySound(clickSound);
-        }
-      }
-
-      else
-      {
-        button.x += 12;
-        ButtonColor.a = 200;
-      }
-
+     Raylib.PlaySound(clickSound);
     }
+   }
+
+   else
+   {
+    button.x += 12;
+    ButtonColor.a = 200;
+   }
+
   }
+ }
 }
