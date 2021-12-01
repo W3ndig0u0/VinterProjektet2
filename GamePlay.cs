fa4fab45002d85745000e851536c54c3ccb4eda8
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2021-12-01 22:12:23 +0100

    Yes

--- GamePlay.cs
+++ GamePlay.cs
@@ -10,13 +10,34 @@ namespace VinterProjektet2
   public void Game()
   {
    Pieces pieces = new Pieces();
-   bool game = true;
 
-   while (game)
+   Image wallpapperMenu = Raylib.LoadImage(@"bg1.png");
+   Raylib.ImageResize(ref wallpapperMenu, 1400, 700);
+   Texture2D wallpapperMenuTextrure = Raylib.LoadTextureFromImage(wallpapperMenu);
+
+
+   Rectangle player = new Rectangle(200, 450, 50, 100);
+   Rectangle enemy1 = new Rectangle(1200, 450, 50, 100);
+
+   while (!Raylib.WindowShouldClose())
    {
+    bool move = Raylib.CheckCollisionRecs(player, enemy1);
+
+    if (move == false)
+    {
+     player.x += 10;
+    }
+    else
+    {
+     player.x += 0;
+    }
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.WHITE);
+    Raylib.DrawTexture(wallpapperMenuTextrure, 0, 0, Color.WHITE);
+
+    Raylib.DrawRectangleRec(player, Color.BLUE);
+    Raylib.DrawRectangleRec(enemy1, Color.BLUE);
 
     Raylib.EndDrawing();
 
